828ed4ad730a0912d343e6e11ec7f7fa7e3fdbb3
Marvin Peña <marvinpena@192.168.1.21>
2025-11-30 23:58:08 +0800

    Use google-chrome path

--- bot.js
+++ bot.js
@@ -526,7 +526,7 @@ async function start_everything(IDENTIFIER_USER, IS_HEADLESS = true, START_IMMED
 	try {
 		browser = await puppeteer.launch({
 			userDataDir: `./${IDENTIFIER_USER}/browser-userdata`,
-			executablePath: '/usr/bin/chromium-browser', // ✅ Use system chromium
+			executablePath: '/usr/bin/google-chrome', // ✅ Use system chromium
 			headless: force_headful ? false : IS_HEADLESS,
 			args: [
 				"--disable-blink-features=AutomationControlled",
